5049d4205defcb1c31bccca858b5bb68c12872c6
Luis Enrique <enriquecalero284@gmail.com>
2024-04-05 13:38:29 -0500

    =VideoIntegrado

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export * from './CardBeneficio/ItemBeneficio.astro';
-export * from './CarruselInfinito/CarruselInfinito.astro';
-export * from './HeroImage/HeroImg.astro';
-export * from './SplitLayout/SplitLayout.astro';
